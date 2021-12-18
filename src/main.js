24775718e51fced62e7827dbb025f69927658a87
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2021-12-18 18:32:31 +0900

    firebase setup

--- src/main.js
+++ src/main.js
@@ -5,6 +5,24 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 new Vue({
   router,
   store,
