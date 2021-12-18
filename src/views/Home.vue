8111d388123a7aadbdc57a4d9714813714025125
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2021-12-18 19:42:32 +0900

    change: jsonに変更→読み込み確認

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{ jobslist }}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import jobsList from "@/assets/jobsList.json";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      jobslist: jobsList,
+    };
   },
 };
 </script>
