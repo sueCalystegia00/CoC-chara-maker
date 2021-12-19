64ece73983b6a7ce6dae67a90b369ed6f2697bce
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2021-12-19 18:07:20 +0900

    update: statusをテーブル表示

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -1,10 +1,39 @@
 <template>
-  <div class="status">ステータス</div>
+  <div class="status">
+    ステータス
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>初期値</th>
+          <th>修正値</th>
+          <th>能力値</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="status in statusList"
+          :key="status.displayName"
+          class="status"
+        >
+          <th>{{ status.displayName }}</th>
+          <td>{{ status.default }}</td>
+          <td>{{ status.revised }}</td>
+          <td>{{ status.define }}</td>
+        </tr>
+      </tbody>
+    </table>
+    {{ status }}
+  </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
   name: "Status",
+  computed: {
+    ...mapState(["statusList"]),
+  },
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    status: {
+    statusList: {
       strength: {
         displayName: "STR",
         default: null,
