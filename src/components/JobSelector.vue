2f30028bfbddf618ca4bbc49e4a9e750dddaa036
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2021-12-20 20:29:35 +0900

    add: ファイル作成

---
+++ src/components/JobSelector.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="jobselector">
+    <h1>職業選択</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JobSelector",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.jobselector {
+  width: 100%;
+  height: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: #000000;
+  color: #ffffff;
+}
+h1 {
+  font-size: 1.3rem;
+}
+</style>
