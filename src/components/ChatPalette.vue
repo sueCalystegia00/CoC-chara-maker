4a57a2dfaf180f2f0119229afe2b1f9bb7e0b78b
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2021-12-22 22:35:05 +0900

    add: ファイルの作成

---
+++ src/components/ChatPalette.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="chat-palette">
+    <h1>CoCキャラクター作成ツール</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChatPalette",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.chat-palette {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+h1 {
+  font-size: 1.3rem;
+}
+</style>
