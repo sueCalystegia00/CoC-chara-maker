150cb27ce7530f1d612f5f6a23f5da6407defdfa
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2021-12-22 13:26:50 +0900

    update: テーブルをグリッド表示

--- src/components/SkillSetter.vue
+++ src/components/SkillSetter.vue
@@ -1,12 +1,63 @@
 <template>
   <div class="skill-setter">
-    <h1>技能ポイントふりふり</h1>
+    <div
+      class="skill-tables"
+      v-for="(category, index) in Object.keys(abilityList)"
+      :key="index"
+    >
+      <div class="title">
+        <h2>{{ category }}</h2>
+      </div>
+      <table class="skill-table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>初期値</th>
+            <th>職業P</th>
+            <th>興味P</th>
+            <th>補正</th>
+            <th>能力値</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(skill, skillName) in abilityList[category]"
+            :key="skillName"
+            class="skill-row"
+          >
+            <th class="displayname">{{ skillName }}</th>
+            <td class="default"></td>
+            <td class="job-point"></td>
+            <td class="int-point"></td>
+            <td class="revised"></td>
+            <td class="value"></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import abilityListData from "@/assets/ abilityList.json";
 export default {
   name: "SkillSetter",
+  data() {
+    return {
+      abilityList: abilityListData,
+    };
+  },
+  computed: {
+    ...mapState(["statusList"]),
+  },
+  mounted() {
+    // ↓不十分，修正予定
+    this.abilityList["戦闘技能"]["回避"].default =
+      this.statusList.commonStatusSheet.dexterity.value * 2;
+    this.abilityList["交渉技能"]["母国語"].default =
+      this.statusList.commonStatusSheet.education.value * 5;
+  },
 };
 </script>
 
@@ -14,15 +65,49 @@ export default {
 <style scoped>
 .skill-setter {
   width: 100%;
-  height: 60px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 10px 20px;
+}
+@media screen and (max-width: 650px) {
+  .skill-setter {
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
+.skill-tables {
+  width: 100%;
   display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.skill-table {
+  width: 100%;
+}
+table,
+thead,
+tbody {
+  width: 100%;
+  text-align: center;
+}
+thead th {
+  font-size: 0.5rem;
+  color: #373737;
+}
+tbody tr {
+  height: 40px;
+}
+tbody th {
+  width: 30%;
+  height: 100%;
   background-color: #000000;
   color: #ffffff;
+  font-size: clamp(0.5rem, 1.8vw, 1rem);
+  font-weight: bold;
 }
-h1 {
-  font-size: 1.3rem;
+tbody td {
+  position: relative;
+  width: 14%;
+  height: 100%;
+  border: 1px solid #cdcdcd;
 }
 </style>
