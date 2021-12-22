8489b5101d1845f76f5a70a83036f40acc4b05c5
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2021-12-22 23:02:56 +0900

    fix: ファイル名修正

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import statusListData from "@/assets/json/statusList.json";
-import abilityListData from "@/assets/json/ abilityList.json";
+import abilityListData from "@/assets/json/abilityList.json";
 
 Vue.use(Vuex);
 
