34dab6460900172dab5aa6f9c2298aac3b210ad8
susanbasket0410 <52482280+susanbasket0410@users.noreply.github.com>
2021-12-27 16:48:16 +0900

    add: キャラシ確認用ページファイルを作成

---
+++ src/views/Character.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="character">キャラシだよ</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Character",
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.character {
+  position: relative;
+  width: 100%;
+  max-width: 1000px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  gap: 20px;
+  box-sizing: border-box;
+}
+
+.describe {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.sticky {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  padding: 10px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  box-sizing: border-box;
+  background-color: #ffffff;
+  z-index: 999;
+}
+</style>
